dcff01ebe441da2ca82585e56370f5009ad6c741
Sujal Shresth <sujshrest@example.com>
2023-12-05 14:50:05 +0530

    Project Completed

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeMessage() {
+            var welcomeMessage = document.getElementById("welcomeMessage");
+            welcomeMessage.innerHTML = "You clicked the button! Welcome messages can change dynamically.";
+        }
